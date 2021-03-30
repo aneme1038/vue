fbd975e22a6dac1d736870a01098db89ed4c49a8
aneme1038 <thriftman25@gmail.com>
2021-03-30 19:28:14 -0400

    transitioned from test data with real world example data

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,17 @@
 
 <script>
 import Store from "./components/Store";
+import { mapActions } from "vuex";
 
 export default {
   name: 'app',
-  components: { Store }
+  components: { Store },
+  methods: {
+    ...mapActions(["getData"])
+  },
+  created() {
+    this.getData();
+  }
 }
 </script>
 
